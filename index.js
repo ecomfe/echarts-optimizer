c1b9eeb3255424cb7281de08692b2391eb65e788
erik <erik168@163.com>
2014-11-03 16:11:21 +0800

    includeShallow, and map geo json

--- index.js
+++ index.js
@@ -12,7 +12,48 @@ var modules = {
         {name: 'echarts/chart/radar', weight: 30},
         {name: 'echarts/chart/chord', weight: 30},
         {name: 'echarts/chart/force', weight: 30},
-        {name: 'echarts/chart/map', weight: 90},
+        {
+            name: 'echarts/chart/map',
+            weight: 90,
+            includeShallow: [
+                'echarts/util/mapData/geoJson/an_hui_geo',
+                'echarts/util/mapData/geoJson/ao_men_geo',
+                'echarts/util/mapData/geoJson/bei_jing_geo',
+                'echarts/util/mapData/geoJson/china_geo',
+                'echarts/util/mapData/geoJson/chong_qing_geo',
+                'echarts/util/mapData/geoJson/fu_jian_geo',
+                'echarts/util/mapData/geoJson/gan_su_geo',
+                'echarts/util/mapData/geoJson/guang_dong_geo',
+                'echarts/util/mapData/geoJson/guang_xi_geo',
+                'echarts/util/mapData/geoJson/gui_zhou_geo',
+                'echarts/util/mapData/geoJson/hai_nan_geo',
+                'echarts/util/mapData/geoJson/hei_long_jiang_geo',
+                'echarts/util/mapData/geoJson/he_bei_geo',
+                'echarts/util/mapData/geoJson/he_nan_geo',
+                'echarts/util/mapData/geoJson/hu_bei_geo',
+                'echarts/util/mapData/geoJson/hu_nan_geo',
+                'echarts/util/mapData/geoJson/jiang_su_geo',
+                'echarts/util/mapData/geoJson/jiang_xi_geo',
+                'echarts/util/mapData/geoJson/ji_lin_geo',
+                'echarts/util/mapData/geoJson/liao_ning_geo',
+                'echarts/util/mapData/geoJson/nei_meng_gu_geo',
+                'echarts/util/mapData/geoJson/ning_xia_geo',
+                'echarts/util/mapData/geoJson/qing_hai_geo',
+                'echarts/util/mapData/geoJson/shang_hai_geo',
+                'echarts/util/mapData/geoJson/shan_dong_geo',
+                'echarts/util/mapData/geoJson/shan_xi_1_geo',
+                'echarts/util/mapData/geoJson/shan_xi_2_geo',
+                'echarts/util/mapData/geoJson/si_chuan_geo',
+                'echarts/util/mapData/geoJson/tai_wan_geo',
+                'echarts/util/mapData/geoJson/tian_jin_geo',
+                'echarts/util/mapData/geoJson/world_geo',
+                'echarts/util/mapData/geoJson/xiang_gang_geo',
+                'echarts/util/mapData/geoJson/xin_jiang_geo',
+                'echarts/util/mapData/geoJson/xi_zang_geo',
+                'echarts/util/mapData/geoJson/yun_nan_geo',
+                'echarts/util/mapData/geoJson/zhe_jiang_geo'
+            ]
+        },
         {name: 'echarts/chart/gauge', weight: 30},
         {name: 'echarts/chart/funnel', weight: 30},
         {name: 'echarts/chart/eventRiver', weight: 10}
@@ -59,7 +100,7 @@ writeFile('analyses/echarts.dependencies', main.dependencies.join('\n'));
 modules.parts.forEach(function (mod) {
     mod.dependencies = analyse(mod.name, 1);
     writeFile(
-        'analyses/' + mod.name.split('/').join('.') + '.dependencies', 
+        'analyses/' + mod.name.split('/').join('.') + '.dependencies',
         mod.dependencies.join('\n')
     );
 });
@@ -81,8 +122,12 @@ main.expectDependencies = union(main.dependencies, extraMainDependencies);
 writeFile('analyses/echarts.dependencies.expect', main.expectDependencies.join('\n'));
 modules.parts.forEach(function (mod) {
     mod.expectDependencies = subtract(mod.dependencies, main.expectDependencies);
+
+    if (mod.includeShallow) {
+        Array.prototype.push.apply(mod.expectDependencies, mod.includeShallow);
+    }
     writeFile(
-        'analyses/' + mod.name.split('/').join('.') + '.dependencies.expect', 
+        'analyses/' + mod.name.split('/').join('.') + '.dependencies.expect',
         mod.expectDependencies.join('\n')
     );
 });
@@ -119,7 +164,7 @@ function writeCompiledCode(file, moduleId, expectDependencies, beforeContent) {
 
 /**
  * 并集
- * 
+ *
  * @return {Array}
  */
 function union(a, b) {
@@ -145,7 +190,7 @@ function union(a, b) {
 
 /**
  * 交集
- * 
+ *
  * @return {Array}
  */
 function intersect(a, b) {
@@ -169,7 +214,7 @@ function intersect(a, b) {
 
 /**
  * 差集
- * 
+ *
  * @return {Array}
  */
 function subtract(a, b) {
